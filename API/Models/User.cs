f52bc53807f0491a859eb7c9308a9a805c939fd4
AndreevMakc <AndreevMakc@yandex.ru>
2023-10-02 21:06:13 +0300

    02 Api project setup, AspNetCore Identity added

---
+++ API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Models;
+
+public class User : IdentityUser
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,15 @@
+using System.Text;
+using API.Data;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +20,49 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//be able to inject JWTService class inside our Controllers
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddRoles<IdentityRole>() // be able to add roles
+    .AddRoleManager<RoleManager<IdentityRole>>() // be able to make use of RoleManager
+    .AddEntityFrameworkStores<Context>() // providing our context
+    .AddSignInManager<SignInManager<User>>() // make use of SignIn manager
+    .AddUserManager<UserManager<User>>() // make use of UserManager to create users
+    .AddDefaultTokenProviders(); // be able to create tokens for email confirmation
+
+//be able to authenticate users using JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // validate the token based on the key we have provided inside appsettings.development.json JWT:Key
+            ValidateIssuerSigningKey = true,
+            // the issuer signing key bases on JWT:Key
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            // the issuer which in here is the api project url we are using
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            // validate the issuer (who ever is issuing the JWT)
+            ValidateIssuer = true,
+            // don't validate audience (angular side)
+            ValidateAudience = false
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +74,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// adding UseAuthentication into out pipeline and this should come before UseAuthorization
+// Authentication verifies the identity of a user or service, and authorization determines their access rights.
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
